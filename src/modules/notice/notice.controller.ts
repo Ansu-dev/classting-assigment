b145893f01b1eb62665f917da45da8aa7c0d4295
Ansu-dev <ansu1007@noutecompany.com>
2023-07-26 11:50:40 +0900

    feat: write notice controller 추가

--- src/modules/notice/notice.controller.ts
+++ src/modules/notice/notice.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { NoticeService } from './notice.service';
+import { AccessTokenGuard } from '../../guard/guard/accessToken.guard';
+import { AdminGuard } from '../../guard/guard/admin.guard';
+import { GetUserId } from 'src/decorator/getUser.decorator';
+import { WriteNoticeRequestDto } from './dto/request/writeNotice.request.dto';
 
 @Controller('notice')
-export class NoticeController {}
+export class NoticeController {
+    constructor(private readonly noticeService: NoticeService) {}
+
+    @Post()
+    @UseGuards(AccessTokenGuard, AdminGuard)
+    async writeNotice(@GetUserId() userId: number, @Body() body: WriteNoticeRequestDto) {
+        return await this.noticeService.writeNotice(userId, body);
+    }
+}
