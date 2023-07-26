22ca6eea7ade044a1e4fa88e888ce1b85e3a99bc
Ansu-dev <ansu1007@noutecompany.com>
2023-07-26 15:39:48 +0900

    feat: notice update controller 추가

--- src/modules/notice/notice.controller.ts
+++ src/modules/notice/notice.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Param, ParseIntPipe, Patch, Post, UseGuards } from '@nestjs/common';
 import { NoticeService } from './notice.service';
 import { AccessTokenGuard } from '../../guard/guard/accessToken.guard';
 import { AdminGuard } from '../../guard/guard/admin.guard';
 import { GetUserId } from 'src/decorator/getUser.decorator';
 import { WriteNoticeRequestDto } from './dto/request/writeNotice.request.dto';
+import { UpdateNoticeRequestDto } from './dto/request/updateNotice.request.dto';
 
 @Controller('notice')
 export class NoticeController {
@@ -13,5 +14,15 @@ export class NoticeController {
     @UseGuards(AccessTokenGuard, AdminGuard)
     async writeNotice(@GetUserId() userId: number, @Body() body: WriteNoticeRequestDto) {
         return await this.noticeService.writeNotice(userId, body);
+    }
+
+    @Patch(':noticeId')
+    @UseGuards(AccessTokenGuard, AdminGuard)
+    async updateNotice(
+        @GetUserId() userId: number,
+        @Param('noticeId', ParseIntPipe) noticeId: number,
+        @Body() body: UpdateNoticeRequestDto,
+    ) {
+        return await this.noticeService.updateNotice(userId, noticeId, body);
     }
 }
