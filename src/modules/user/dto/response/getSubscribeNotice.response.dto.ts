0c25fda2eb1bfbee74cd541a979ee26c86ccead7
Ansu-dev <ansu1007@noutecompany.com>
2023-07-27 16:21:27 +0900

    feat: 소식 모아보기 response dto 세팅

--- src/modules/user/dto/response/getSubscribeNotice.response.dto.ts
+++ src/modules/user/dto/response/getSubscribeNotice.response.dto.ts
@@ -20,3 +20,19 @@ export class GetSubscribeNoticeData {
     @ApiProperty({ description: '소식 등록 날짜' })
     createdAt: Date;
 }
+
+export class GetSubscribeNoticeItem {
+    @ApiProperty({ type: [GetSubscribeNoticeData] })
+    items: GetSubscribeNoticeData[];
+
+    @ApiProperty()
+    count: number;
+}
+
+export class GetSubscribeNoticeResDto {
+    @ApiProperty({ default: 1 })
+    resultCode: number;
+
+    @ApiProperty({ type: GetSubscribeNoticeItem })
+    data: GetSubscribeNoticeItem;
+}
