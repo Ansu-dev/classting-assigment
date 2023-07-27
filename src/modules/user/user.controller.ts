6bf6f89278eb6103871cbf0e63c5fe07eeb68a35
Ansu-dev <ansu1007@noutecompany.com>
2023-07-27 16:09:46 +0900

    feat: 구독중인 학교 소식 controller 추가

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
+import { AccessTokenGuard } from '../../guard/guard/accessToken.guard';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import { GetUserId } from 'src/decorator/getUser.decorator';
+import { GetSubscribeNoticeQueryDto } from './dto/request/getSubscribeNotice.query.dto';
 
 @Controller('user')
 export class UserController {
     constructor(private readonly userService: UserService) {}
+
+    @Get('subcribe/notice')
+    @UseGuards(AccessTokenGuard)
+    @ApiBearerAuth('authorization')
+    @ApiOperation({ summary: '구독 중인 학교 페이지 목록' })
+    async getSubscribeNotices(@GetUserId() userId: number, @Query() query: GetSubscribeNoticeQueryDto) {
+        return await this.userService.getSubscribeNotices(userId, query);
+    }
 }
